0eae090503b19ac5f9f9728da425174eb6cb3043
dipanshu447 <dipanshusahu447@gmail.com>
2025-06-28 09:48:31 +0530

    chore: improve help message with examples and add GitHub issue link

--- utils/flags.js
+++ utils/flags.js
@@ -4,17 +4,23 @@ export function help() {
   console.log(`
 Usage:
   create-discord-bot <folder-name>
-
-Note: If no <folder-name> is provided, you will be prompted to enter one interactively.
+  create-discord-bot .              Scaffold in current directory
+  create-discord-bot -y .           Use defaults in current directory (non-interactive)
 
 Options:
-  --help, -h      Show this help message
-  --version, -v   Show CLI version
-  --yes, -y       Generate with default options (JavaScript, my-discord-bot)
-  --pkgname, -p   Set the package name
-  --lang, -l      Set the language (e.g., -l js)
-`);
+  --help, -h        Show this help message
+  --version, -v     Show CLI version
+  --yes, -y         Generate with default options (JavaScript, my-discord-bot)
+  --pkgname, -p     Set the package name (e.g., -p cool-bot)
+  --lang, -l        Set the language (e.g., -l js)
 
+Notes:
+  - If no <folder-name> is provided, you will be prompted to enter one.
+  - You can use '.' to initialize the bot in the current directory.
+
+Having issues? Feel free to open an issue at:
+https://github.com/dipanshu447/create-discord-bot/issues
+`);
   process.exit(0);
 }
 
